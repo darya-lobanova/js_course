4caaade66f7f3f220a360cabd61c1c17aaa1a1cf
Darya Lobanova <daryalobanova7@gmail.com>
2018-05-07 22:48:24 +0300

    second commit

---
+++ lesson_11/index.js
@@ -0,0 +1,38 @@
+﻿ let a = 2;
+ let b = 8;
+ let operation = "pow";
+  let res;
+
+ switch (operation) {
+     
+	case "add":
+    		res = a + b;
+		console.log(res);
+	break;
+	case "sub":
+      		res = a - b;
+		console.log(res);
+		break;
+
+ 	case "mult":
+      		res = a * b;
+		console.log(res);
+		break;
+
+	case "div":
+	 if (b !== 0) {
+      			res = a / b;
+			console.log(res);
+ 		} 
+		else {
+			console.log("can not be divided by zero");
+		}
+	break;
+
+	case "pow":
+     		res = Math.pow(a, b);
+		console.log(res);
+		break;
+	default:
+ 	console.log( "Please enter correct data" )
+ }
