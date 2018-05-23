08aa370291d2b3e70c3250fa80c07671a90cbf70
Darya Lobanova <daryalobanova7@gmail.com>
2018-05-23 19:35:16 +0300

    changes in lesson_13

--- lesson_13/index.js
+++ lesson_13/index.js
@@ -6,20 +6,16 @@ function removeKeys1(obj1, arr1) {
 	}
 	return obj1;
 }
-
-
 function removeKeys2(obj2, arr2) {
 	for (let key of arr2) {
 		if (key in obj2) {
 			delete obj2[key];
 		}
 	}
-	let newObj = new Object(obj2);
-	
+	let newObj = Object.assign({}, obj2);
+
 	return newObj;
 }
-
-
 function absDiff(a) {
 	return (b) => Math.abs(a - b);
 }
