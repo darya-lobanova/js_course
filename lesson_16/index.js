ded42202b5e846900797f905743ba51891192370
Darya Lobanova <daryalobanova7@gmail.com>
2018-05-24 23:42:28 +0300

    changed lesson_16

--- lesson_16/index.js
+++ lesson_16/index.js
@@ -6,6 +6,9 @@ const stop = document.getElementById('stop');
 const numbers = document.getElementById('numbers');
 const second = document.getElementById('second');
 const seconds = document.getElementById('seconds');
+const x1 = document.getElementById('x1');
+const x2 = document.getElementById('x2');
+const x3 = document.getElementById('x3');
 
 
 for (let i = 1; i <= n; i += 1) {
@@ -22,6 +25,7 @@ let num = 0;
 let counter = 0;
 let interval;
 let speed = 1000;
+let check = false;
 
 second.appendChild(document.createTextNode(num));
 seconds.appendChild(document.createTextNode('s'));
@@ -38,7 +42,7 @@ function count () {
 
 		if (counter < n) {
 			counter += 1;
-			document.getElementById(`span_${counter}`).style.backgroundColor = 'lightgreen';
+			document.getElementById(`span_${counter}`).style.backgroundColor = 'lightblue';
 		} else if (counter === n) {
 			counter += n + 1;
 			document.getElementById(`span_${counter - n - 1}`).style.backgroundColor = 'lightpink';
@@ -48,17 +52,26 @@ function count () {
 			document.getElementById(`span_${counter - n}`).style.backgroundColor = 'lightpink';
 		} else if (counter === n + 1) {
 			counter = 0;
-			document.getElementById(`span_${counter + 1}`).style.backgroundColor = 'lightgreen';
+			document.getElementById(`span_${counter + 1}`).style.backgroundColor = 'lightblue';
 			counter += 1;
 		}
 	}, speed)
 	start.setAttribute('disabled', 'true');
 
 	stop.addEventListener('click', function(){
-		clearInterval(interval);		
+		counter = 0;
+		clearInterval(interval);
+		second.innerHTML = 0;		
 		start.removeAttribute('disabled');
 		second.innerHTML = 0;
 		num = 0;
+		let numb = numbers.children;
+		for (let i = 1; i < numb.length - 1; i++ ) {
+			numb[i].style.backgroundColor = 'white';
+		}
+		letS.innerHTML = 's';
+		startB.removeAttribute('disabled');
+		check = false;
 	})
 
 	pause.addEventListener('click', function() {
@@ -68,3 +81,19 @@ function count () {
 }
 start.addEventListener('click', count);
 
+
+x1.addEventListener('click', function(){
+	speed = 1000;
+	clearInterval(interval);
+	if (num > 0 && check === false) count();
+});
+x2.addEventListener('click', function(){
+	speed = 500;
+	clearInterval(interval);
+	if (num > 0 && check === false) count();
+});
+x3.addEventListener('click', function(){
+	speed = 1000 / 3;
+	clearInterval(interval);
+	if (num > 0 && check === false) count();
+})
