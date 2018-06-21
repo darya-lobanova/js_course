c37e78ce32c67498336bf859a63ee4cf836a5f27
Darya Lobanova <daryalobanova7@gmail.com>
2018-06-22 00:28:51 +0300

    changes in lesson_19

--- lesson_19/index.js
+++ lesson_19/index.js
@@ -1,13 +1,41 @@
-function checkAuth() {  
-    return new Promise(function(resolve, reject) {
+const checkAuth = () => {
+    return new Promise(function(resolve, reject){
         setTimeout(() => {
-            resolve( {name: 'Max'} );
-            reject( null );
-        }, 2000);       
-    })
-    .then(user => {
-        setTimeout(() => {
-            console.log(user.name);
+            resolve({isAuth: true});
         }, 2000);
     })
-}
+};
+  
+
+const getUser = (authInfo) => {
+    return new Promise(function(resolve, reject){
+        if (!authInfo.isAuth) {
+            resolve(null);
+            return;
+        }
+        setTimeout(() => {
+            reject({name: "Max"});
+        }, 2000);
+    })
+};
+checkAuth()
+    .then((res) =>{
+        return getUser(res);
+    })
+    .then((res) =>{
+        console.log(res);
+    });
+
+// function checkAuth() {  
+//     return new Promise(function(resolve, reject) {
+//         setTimeout(() => {
+//             resolve( {name: 'Max'} );
+//             reject( null );
+//         }, 2000);       
+//     })
+//     .then(user => {
+//         setTimeout(() => {
+//             console.log(user.name);
+//         }, 2000);
+//     })
+// }
